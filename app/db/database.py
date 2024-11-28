5dc6aa58181d66383db7098648401c511ffc9ef4
EboTheFirst <ebokak5@gmail.com>
2024-11-28 11:39:50 +0000

    Changes for ec2

--- app/db/database.py
+++ app/db/database.py
@@ -2,8 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:082001@localhost/credit"
-# DATABASE_URL = "postgresql://itc:1234@localhost/credit"
+# DATABASE_URL = "postgresql://postgres:082001@localhost/credit"
+DATABASE_URL = "postgresql://itc:082001@localhost/credit"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
